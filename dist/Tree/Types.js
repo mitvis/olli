3d4325a9745b792d2a4f2e85bb1f693296d3a50a
Matt-Blanco <mattmblanco@gmail.com>
2022-05-10 15:17:26 -0400

    added gitignore

--- dist/Tree/Types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/*
-TODO:
-  - Expand type system to include more meta-data that can be accessed and allow each node to be easily expanded upon
-  - Possibly create different types for different nodes that can specifically be accessed in functions to get more information
-*/
