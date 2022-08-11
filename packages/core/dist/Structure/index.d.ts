f6ca51d8e0b5a19912af2562406e0a2b8c315541
Matt-Blanco <mattmblanco@gmail.com>
2022-08-11 10:44:08 -0400

    removed dist

--- packages/core/dist/Structure/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { OlliVisSpec } from "olli-adapters/src/Types";
-import { AccessibilityTreeNode } from "./Types";
-/**
- * Constructs an {@link AccessibilityTreeNode} based off of a generalized visualization
- * @param olliVisSpec the {@link Chart} or {@link CompositeChart} to transform into a tree
- * @returns The transormed {@link AccessibilityTreeNode}
- */
-export declare function olliVisSpecToTree(olliVisSpec: OlliVisSpec): AccessibilityTreeNode;
-//# sourceMappingURL=index.d.ts.map
