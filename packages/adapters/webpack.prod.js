79efb0097ea7cf90f2649cfa45c05722012254d0
Jonathan Zong <jzong@mit.edu>
2023-05-13 23:58:24 -0400

    live reloading in dev

--- packages/adapters/webpack.prod.js
+++ packages/adapters/webpack.prod.js
@@ -1,6 +1,19 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const exec = require('child_process').exec;
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    {
+      apply: (compiler) => {
+        compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+          exec('cp ./dist/adapters.js ../../docs/olli/adapters.js', (err, stdout, stderr) => {
+            if (stdout) process.stdout.write(stdout);
+            if (stderr) process.stderr.write(stderr);
+          });
+        });
+      }
+    }
+  ]
 });
--- packages/core/webpack.dev.js
+++ packages/core/webpack.dev.js
@@ -1,7 +1,20 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const exec = require('child_process').exec;
 
 module.exports = merge(common, {
   mode: 'development',
   watch: true,
+  plugins: [
+    {
+      apply: (compiler) => {
+        compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+          exec('cp ./dist/olli.js ../../docs/olli/olli-dev.js', (err, stdout, stderr) => {
+            if (stdout) process.stdout.write(stdout);
+            if (stderr) process.stderr.write(stderr);
+          });
+        });
+      }
+    }
+  ]
 });
