b56f7b6a9fc97d13ad69fe7a7ad958b4f07dcd41
Jonathan Zong <jzong@mit.edu>
2023-06-02 16:29:30 -0400

    cleanup examples

--- packages/core/src/description/index.ts
+++ packages/core/src/description/index.ts
@@ -1,6 +1,6 @@
 import { OlliValue } from '../Types';
 import { OlliSpec } from '../Types';
-import { ElaboratedOlliNode } from '../Structure/Types';
+import { ElaboratedOlliNode, OlliNode } from '../Structure/Types';
 import { getBins } from '../util/bin';
 import { getDomain, getFieldDef } from '../util/data';
 import { selectionTest } from '../util/selection';
@@ -21,48 +21,58 @@ export function generateDescriptions(olliSpec: OlliSpec, tree: ElaboratedOlliNod
 }
 
 export function nodeToDescription(node: ElaboratedOlliNode, olliSpec: OlliSpec): string {
-  const index = (node.parent?.children.indexOf(node) || 0) + 1;
-  const siblings = (node.parent?.children || []).length;
+  const index = `${(node.parent?.children.indexOf(node) || 0) + 1} of ${(node.parent?.children || []).length}. `;
+  const description = olliSpec.description?.concat(' ') || '';
+  const chartType = () => {
+    if (olliSpec.mark) {
+      if (olliSpec.mark === 'point' && olliSpec.axes?.length === 2) {
+        if (olliSpec.axes.every((a) => getFieldDef(a.field, olliSpec).type === 'quantitative')) {
+          return 'scatterplot';
+        } else if (
+          olliSpec.axes.find((a) => getFieldDef(a.field, olliSpec).type === 'quantitative') &&
+          !olliSpec.axes.find((a) => getFieldDef(a.field, olliSpec).type === 'temporal')
+        ) {
+          return 'dotplot';
+        }
+      }
+      return `${olliSpec.mark} chart`;
+    } else {
+      return 'dataset';
+    }
+  };
+  const chartTypePrefix = (node: ElaboratedOlliNode): string => {
+    if (node && 'groupby' in node && node.nodeType === 'root') {
+      if (olliSpec.mark === 'line') {
+        return 'multi-series ';
+      } else {
+        return 'multi-view ';
+      }
+    }
+    return '';
+  };
+  const axes = olliSpec.axes.map((a) => a.title || a.field).join(' and ');
+
   switch (node.nodeType) {
     case 'root':
-      if ('groupby' in node && olliSpec.mark === 'line') {
-        return `${olliSpec.description?.concat(' ') || ''}A multi-series line chart with 5 lines for ${
+      if ('groupby' in node) {
+        return `${description}A ${chartTypePrefix(node)}${chartType()} with ${node.children.length} views for ${
           node.groupby
-        }, with axes ${olliSpec.axes.map((a) => a.title || a.field).join(' and ')}.`;
-      }
-      if (
-        olliSpec.mark === 'point' &&
-        olliSpec.axes?.length === 2 &&
-        olliSpec.axes.every((a) => getFieldDef(a.field, olliSpec).type === 'quantitative')
-      ) {
-        return `${olliSpec.description?.concat(' ') || ''}A scatterplot with axes ${olliSpec.axes
-          .map((a) => a.title || a.field)
-          .join(' and ')}.`;
+        }, with axes ${axes}.`;
       }
       if (olliSpec.mark && olliSpec.axes?.length) {
-        return `${olliSpec.description?.concat(' ') || ''}A ${olliSpec.mark} chart with axes ${olliSpec.axes
-          .map((a) => a.title || a.field)
-          .join(' and ')}.`;
+        return `${olliSpec.description?.concat(' ') || ''}A ${chartType()} with axes ${axes}.`;
       }
       if (olliSpec.mark) {
-        return `${olliSpec.description?.concat(' ') || ''}A ${olliSpec.mark} chart.`;
+        return `${olliSpec.description?.concat(' ') || ''}A ${chartType()}.`;
       }
       const fields = olliSpec.fields.map((f) => f.field);
       return `A dataset with ${fields.length} fields${fields.length <= 3 ? ' ' + [...fields].join(', ') : ''}.`;
     case 'facet':
+      const facetName = olliSpec.mark === 'line' ? 'line' : olliSpec.mark ? chartType() : 'facet';
       if ('predicate' in node && 'equal' in node.predicate) {
-        if (olliSpec.mark === 'line') {
-          return `${index} of ${siblings}. A line titled ${node.predicate.equal}, with axes ${olliSpec.axes
-            .map((a) => a.title || a.field)
-            .join(' and ')}.`;
-        }
-        return `${index} of ${siblings}. A ${olliSpec.mark} chart titled ${
-          node.predicate.equal
-        }, with axes ${olliSpec.axes.map((a) => a.title || a.field).join(' and ')}.`;
+        return `${index}A ${facetName} titled ${node.predicate.equal}, with axes ${axes}.`;
       }
-      return `${index} of ${siblings}. A facet with axes ${olliSpec.axes
-        .map((a) => a.title || a.field)
-        .join(' and ')}.`;
+      return `${index}A ${facetName} with axes ${axes}.`;
     case 'xAxis':
     case 'yAxis':
     case 'legend':
@@ -93,34 +103,18 @@ export function nodeToDescription(node: ElaboratedOlliNode, olliSpec: OlliSpec):
       if ('predicate' in node) {
         const selection = selectionTest(olliSpec.data, node.fullPredicate);
         if ('range' in node.predicate) {
-          return `${index} of ${siblings}. ${fmtValue(node.predicate.range[0])} to ${fmtValue(
-            node.predicate.range[1]
-          )}. ${selection.length} values.${instructions}`;
-        } else if ('equal' in node.predicate) {
-          return `${index} of ${siblings}. ${fmtValue(node.predicate.equal as OlliValue)}. ${
+          return `${index}${fmtValue(node.predicate.range[0])} to ${fmtValue(node.predicate.range[1])}. ${
             selection.length
           } values.${instructions}`;
+        } else if ('equal' in node.predicate) {
+          return `${index}${fmtValue(node.predicate.equal as OlliValue)}. ${selection.length} values.${instructions}`;
         }
       }
     case 'other':
       if ('groupby' in node) {
-        if (!node.parent) {
-          const fields = olliSpec.fields.map((f) => f.field);
-          return `A dataset with ${fields.length} fields${
-            fields.length <= 3 ? ' ' + [...fields].join(', ') : ''
-          }, grouped by ${node.groupby}.`;
-        }
         return `${node.children.length} groups, grouped by ${node.groupby}.`;
       } else if ('predicate' in node) {
-        let predicateDescription;
-        if ('range' in node.predicate) {
-          predicateDescription = `is between ${fmtValue(node.predicate.range[0])} to ${fmtValue(
-            node.predicate.range[1]
-          )}`;
-        } else if ('equal' in node.predicate) {
-          predicateDescription = `is ${fmtValue(node.predicate.equal as OlliValue)}`;
-        }
-        return `${index} of ${siblings}. ${node.predicate.field} ${predicateDescription}.`;
+        return `${index}${node.predicate.field} ${predicateToDescription(node.predicate)}.`;
       }
   }
 }
