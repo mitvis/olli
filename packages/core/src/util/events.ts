4dcb354d67d79632be11ed54e02130b579113830
Jonathan Zong <jzong@mit.edu>
2023-08-19 21:49:17 -0400

    update event check

--- packages/core/src/util/events.ts
+++ packages/core/src/util/events.ts
@@ -1,9 +1,10 @@
 export const nodeIsTextInput = (activeElement: Element | null): boolean => {
   switch (activeElement?.nodeName) {
     case 'INPUT':
+      return !['range', 'button', 'checkbox', 'number'].includes(activeElement.getAttribute('type') || '');
     case 'TEXTAREA':
-    case 'SELECT':
-    case 'OPTION':
+      // case 'SELECT':
+      // case 'OPTION':
       return true;
   }
   if (typeof activeElement?.getAttribute('contenteditable') === 'string') return true;
